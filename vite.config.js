91a71b6471d6f94254fdc44cabf5aff0d01fe191
Satik37 <satmil@tiscali.it>
2026-06-03 18:15:48 +0200

    setting vite with tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+// per usare __dirname in ambiente ES module
+import { fileURLToPath } from 'node:url';
+
+// ricrea __dirname in ambiente ES module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
