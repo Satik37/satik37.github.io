6fdaaab568b6b393597a46b02f8203ea4cda48e5
Satik37 <satmil@tiscali.it>
2026-06-08 11:32:32 +0200

    improved navbar and set for web

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/Button';
+
 const navLinks = [
     {href: '#about', label: 'About'},
     {href: '#projects', label: 'Projects'},
@@ -9,17 +11,25 @@ export const Navbar = () => {
     return (
         <header className='fixed top-0 left-0 right-0 bh-transparent py-5'>
             <nav className='container mx-auto px-6 flex items-center justify-between'>
-                <a href='#' className='text-xl font-bold tracking-tight '>
-                    Satik37<span>.</span>
+                <a href='#' className='text-xl font-bold tracking-tight hover:text-primary'>
+                    Satik37<span className='text-primary'>.</span>
                 </a>
                 
                 {/* Desktop Nav */}
+                <div className='flex items-center gap-1'>
+                    <div className='glass rounded-full px-2 py-1 flex items-center gap-1'>
+                        {navLinks.map((link, index) => (
+                            // <a href={link.href} key={index} className='hover:text-primary'>
+                                <a href={link.href} key={index} className='px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface'>
+                                {link.label}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+
+                {/* CallToAction Button */}
                 <div>
-                    {navLinks.map((link, index) => (
-                        <a href={link.href} key={index}>
-                            {link.label}
-                        </a>
-                    ))}
+                    <Button size="sm">Contact Me</Button>
                 </div>
             </nav>
         </header>
