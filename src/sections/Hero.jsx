3f244e41534f45088058fe61ee2de322cdb65c32
Satik37 <satmil@tiscali.it>
2026-06-09 16:47:03 +0200

    added temporary img

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -114,6 +114,30 @@ export const Hero = () => {
                         </div>
                     </div>
                     {/* Right Column - Pic */}
+                    <div className='relative animate-fade-in animation-delay-300'>
+                        {/* Hero Image */}
+                        <div className='relative max-2-md mx-auto'>
+                            <div
+                                className='absolute inset-0
+                                    rounded-3xl bg-linear-to-br
+                                    from-primary/30
+                                    via-transparent
+                                    to-primary/10
+                                    blur-2xl
+                                    animate-pulse'
+                            />
+                            <div className='relative glass rounded-3xl p-2 glow-border'>
+                                <img
+                                    src='/profile_photo.png'
+                                    alt='Saturnas C.M.'
+                                    className='w-full aspect-ratio: 4/5 object-cover rounded-2xl'
+                                />
+
+                                {/* Floating Badge */}
+                                {/* Stats Badge */}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
