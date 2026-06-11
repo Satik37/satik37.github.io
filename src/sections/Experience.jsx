4dbafefabd5aac85060e448f4df3496aa199f04f
Satik37 <satmil@tiscali.it>
2026-06-11 14:23:00 +0200

    experiences array

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,3 +1,38 @@
+const experiences = [
+    {
+        period: '',
+        role: '',
+        company: '',
+        description: '',
+        technologies: [],
+        current: false,
+    },
+    {
+        period: '',
+        role: '',
+        company: '',
+        description: '',
+        technologies: [],
+        current: true,
+    },
+    {
+        period: '',
+        role: '',
+        company: '',
+        description: '',
+        technologies: [],
+        current: false,
+    },
+    {
+        period: '',
+        role: '',
+        company: '',
+        description: '',
+        technologies: [],
+        current: false,
+    },
+]
+
 export const Experience = () => {
     return <section></section>
 }
